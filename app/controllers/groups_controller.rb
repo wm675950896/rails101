8cebcb4f64edc46cc42c535589c4ff91fbd6dfdf
王淼 <miao.wang02@hand-china.com>
2019-07-24 14:42:28 +0800

    add validation to title apply to edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,4 +4,43 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
 
+  def new
+    @group = Group.new
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to groups_path, notice: "Update"
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "delete"
+    redirect_to groups_path
+  end
+
+  private
+    def group_params
+      params.require(:group).permit(:title, :description)
+    end
+
 end
