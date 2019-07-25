7644480baa6f377d380ae417b307650f082656d0
王淼 <miao.wang02@hand-china.com>
2019-07-25 15:55:43 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :destroy, :update]
 
   def index
@@ -30,6 +30,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.user = current_user
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -39,6 +40,28 @@ class GroupsController < ApplicationController
   def destroy
     @group.destroy
     redirect_to groups_path, alert: "Group deleted"
+  end
+
+  def join
+    @group = Group.find(params[:id])
+    unless current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "Join"
+    else
+      flash[:warning] = "already join"
+    end
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:notice] = "quit"
+    else
+      flash[:warning] = "not team"
+    end
+    redirect_to group_path(@group)
   end
 
   private
